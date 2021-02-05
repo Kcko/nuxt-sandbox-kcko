28fb154eb4e470049716fec0baae7c1c843ffb4c
Roman Janko <admin@rjwebdesign.cz>
2021-02-05 17:41:26 +0100

    inital

---
+++ components/NavigationPanel.vue
@@ -0,0 +1,9 @@
+<template>
+<div>
+    <nuxt-link to="/">Home</nuxt-link>
+    <nuxt-link to="/romca">Romca</nuxt-link>
+</div>
+</template>
+
+<style>
+</style>
---
+++ pages/romca.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">ROMCA :-)</h1>
+      <navigation-panel />
+    </div>
+  </div>
+</template>
+
+<script>
+import NavigationPanel from '~/components/NavigationPanel'
+export default {
+  components: {
+    NavigationPanel,
+  },
+  data() {
+    return {
+      count: "2323fff",  // ctr s a hovno
+      foo: 'cus'
+    }
+  },
+  methods: {
+    test() {
+      return "fsdfasdf"; // tady to samy
+    },
+  },
+}
+</script>
+
+<style></style>
